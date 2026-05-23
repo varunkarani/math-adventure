4c3d5097e84a2940bb014ae5719965ae10dbc31b
varunkarani <78227969+varunkarani@users.noreply.github.com>
2026-05-23 16:35:15 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener("install", (e) => {
+
+  e.waitUntil(
+
+    caches.open("math-adventure").then((cache) => {
+
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js"
+      ]);
+
+    })
+
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+
+  e.respondWith(
+
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+
+  );
+});
