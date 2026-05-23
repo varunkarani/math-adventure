993cbfbe7f42082daadc6938a5b4d83a6290451d
varunkarani <78227969+varunkarani@users.noreply.github.com>
2026-05-24 00:03:42 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -494,5 +494,29 @@ updateStats();
     });
 
   }
+  /* ========================= */
+  /* PREVENT IOS DOUBLE TAP */
+  /* ========================= */
 
+  let lastTouchEnd = 0;
+
+  document.addEventListener(
+    "touchend",
+    (event)=>{
+
+      const now =
+        Date.now();
+
+      if(
+        now - lastTouchEnd <= 300
+      ){
+
+        event.preventDefault();
+      }
+
+      lastTouchEnd = now;
+
+    },
+    { passive:false }
+  );
 });
