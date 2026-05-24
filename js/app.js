0a6a3f0105814f575e875ad01b6eb7c60b6b65ec
varunkarani <78227969+varunkarani@users.noreply.github.com>
2026-05-24 08:54:33 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -413,27 +413,4 @@ bedtimeBtn.addEventListener(
 
 }
 
-/* ========================= /
-/ PREVENT IOS DOUBLE TAP /
-/ ========================= */
-
-let lastTouchEnd = 0;
-
-document.addEventListener(
-“touchend”,
-(event)=>{
-
-  const now =
-    Date.now();
-  if(
-    now - lastTouchEnd <= 300
-  ){
-    event.preventDefault();
-  }
-  lastTouchEnd = now;
-},
-{ passive:false }
-
-);
-
 });
